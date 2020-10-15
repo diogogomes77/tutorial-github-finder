fb0679c884e8b076285257a71bfbbcfbcdf2910c
Diogo Gomes <diogo.gomes77@gmail.com>
2020-10-15 14:46:36 +0100

    users list (using classes)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React from "react";
+import Navbar from "./components/layout/Navbar";
+
+import Users from "./components/users/Users";
 import "./App.css";
 
 class App extends React.Component {
   render() {
-    const name = "John Doe";
-    const loading = false;
-    const showName = false;
     return (
       <div className="App">
-        {loading ? (
-          <h1>Loading...</h1>
-        ) : (
-          <h1>Hello {showName && name.toUpperCase()}</h1>
-        )}
+        <Navbar />
+        <div className="container">
+          <Users />
+        </div>
       </div>
     );
   }
